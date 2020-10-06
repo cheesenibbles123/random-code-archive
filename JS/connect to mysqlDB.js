47d18906029da5fbbde3bae28ce5d24936a028b0
Archie the Inventor <56054349+cheesenibbles123@users.noreply.github.com>
2020-10-06 20:28:32 +0100

    Create connect to mysqlDB.js

---
+++ JS/connect to mysqlDB.js
@@ -0,0 +1,28 @@
+mysqlLoginData = {
+  "host" : "255.255.255.255",
+  "user" : "root",
+  "password" : "password",
+  "database" : "myDatabase"
+}
+
+function interactWithDB(){
+
+  var connection = mysql.createConnection({
+			host : mysqlLoginData.host,
+			user : mysqlLoginData.user,
+			password : mysqlLoginData.password,
+			database : mysqlLoginData.database,
+		});
+			
+		connection.connect(err => {
+			if(err) console.log(err);
+		});
+
+		connection.query(`SELECT * sampleTable`, (err,rows) => {
+      // Do something with data
+		});
+
+		setTimeout(function(){
+			connection.end();
+		},3000);
+ }
