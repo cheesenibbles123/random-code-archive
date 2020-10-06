6fdb9b36ee3035409ecd0e90e5f1b2470559b865
Archie the Inventor <56054349+cheesenibbles123@users.noreply.github.com>
2020-10-06 20:32:21 +0100

    Create discordjs raw event switch.js

---
+++ JS/discordjs raw event switch.js
@@ -0,0 +1,35 @@
+switch (event.t){
+		case "CHANNEL_CREATE":
+			// INSERT CODE
+			break;
+		case "CHANNEL_DELETE":
+			// INSERT CODE
+			break;
+		case "CHANNEL_PINS_UPDATE":
+			// INSERT CODE
+			break;
+		case "GUILD_BAN_ADD":
+			// INSERT CODE
+			break;
+		case "GUILD_BAN_REMOVE":
+			// INSERT CODE
+			break;
+		case "GUILD_MEMBER_REMOVE":
+			// INSERT CODE
+			break;
+		case "GUILD_ROLE_CREATE":
+			// INSERT CODE
+			break;
+		case "GUILD_ROLE_DELETE":
+			// INSERT CODE
+			break;
+		case "MESSAGE_REACTION_ADD":
+			// INSERT CODE
+			break;
+		case "MESSAGE_REACTION_REMOVE":
+      // INSERT CODE
+			break;
+		default:
+      // INSERT CODE
+			break;
+	}
