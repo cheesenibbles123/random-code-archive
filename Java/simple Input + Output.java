b8eda195bd7acf61a433ca1f29a71bae96bfbf79
Archie the Inventor <56054349+cheesenibbles123@users.noreply.github.com>
2020-10-08 14:19:15 +0100

    Create simple Input + Output.java

---
+++ Java/simple Input + Output.java
@@ -0,0 +1,6 @@
+public static void main(string[] args){
+  Scanner input = new Scanner(System.in);
+  System.out.println("Please enter a number:");
+  int inVariable = input.nextInt();
+  System.out.println("You input: " + inVariable);
+}
