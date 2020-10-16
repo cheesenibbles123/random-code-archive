67d7f647d4998857375cebb81e7da5d0af65b230
Archie the Inventor <56054349+cheesenibbles123@users.noreply.github.com>
2020-10-16 11:38:35 +0100

    Create simpleSwitcher.cs

---
+++ c-sharp/simpleSwitcher.cs
@@ -0,0 +1,13 @@
+string shipType = "cruiser";
+
+switch (shipType)
+{
+  case "cruiser":
+    logLow("Cruiser found!");
+    break;
+  case "brig":
+    logLow("Brig found!");
+    break;
+  default:
+    break;
+}
