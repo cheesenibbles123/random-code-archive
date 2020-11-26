262dbf77ed73ec9f2c20b6f73ee3bb473640c541
Archie the Inventor <56054349+cheesenibbles123@users.noreply.github.com>
2020-11-26 12:52:08 +0000

    Create cheapstake-login.py

---
+++ python/cheapstake-login.py
@@ -0,0 +1,9 @@
+username = "user"
+password = "password"
+
+inputPassword = input("Please enter your password: ")
+inputUsername = input("Please enter your username: ")
+
+if username == inputUsername:
+  if password == inputPassword"
+    print("Login successful")
