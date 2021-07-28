1e60e09624a86f5e6e0e40541beeb8fdcebb751c
cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-07-29 00:19:10 +0100

    Added some basic lua code

---
+++ lua/Basic structures.lua
@@ -0,0 +1,51 @@
+[[--
+  Comment block
+  --]]
+
+[[--
+  Basic function layout
+  --]]
+
+function myFunction(variable)
+    [[-- Do stuff --]]
+end
+
+[[--
+  If Statement
+ --]]
+
+if input < 10 then
+    [[-- Do stuff --]]
+else if input > 10 then
+    [[-- Do stuff --]]
+else
+    [[-- Do stuff --]]
+end
+
+[[--
+  for loop
+ --]]
+
+local i
+local num = 10
+local count = 0
+
+for i=1, num do
+    count = count + 1
+end
+
+[[--
+  while loop
+ --]]
+
+while true do
+    [[-- Do stuff --]]
+end
+
+[[--
+  null check
+ --]]
+
+if variable == nil then
+    [[-- Do stuff --]]
+end
